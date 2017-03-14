29a34c879d91829cff889efd3b855c20e7a50da3
sicacho <trustme013@gmail.com>
2017-03-14 23:34:00 +0700

    upload controller

--- src/main/java/com/hac/controller/IndexController.java
+++ src/main/java/com/hac/controller/IndexController.java
@@ -2,7 +2,15 @@ package com.hac.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.annotation.MultipartConfig;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created by Administrator on 3/13/2017.
@@ -12,5 +20,26 @@ public class IndexController {
   @RequestMapping(value = "/",produces = "text/html")
   public @ResponseBody String index() {
     return "test";
+  }
+
+  @RequestMapping(value="/upload", method= RequestMethod.POST)
+
+  public @ResponseBody String handleFileUpload(
+      @RequestParam("file") MultipartFile file){
+    String name = "video";
+    if (!file.isEmpty()) {
+      try {
+        byte[] bytes = file.getBytes();
+        BufferedOutputStream stream =
+            new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+        stream.write(bytes);
+        stream.close();
+        return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+      } catch (Exception e) {
+        return "You failed to upload " + name + " => " + e.getMessage();
+      }
+    } else {
+      return "You failed to upload " + name + " because the file was empty.";
+    }
   }
 }
---
+++ src/main/java/com/hac/service/ProducerService.java
@@ -0,0 +1,7 @@
+package com.hac.service;
+
+/**
+ * Created by Administrator on 3/14/2017.
+ */
+public interface ProducerService {
+}
