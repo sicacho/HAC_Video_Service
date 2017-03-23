1a00398fd7821069b79bebc0d642e2721c5dc0f0
truongnhukhang <truong.khang@orientsoftware.net>
2017-03-23 15:37:19 +0700

    add rabbitMQ library

---
+++ model/src/main/java/com/hac/domain/Video.java
@@ -0,0 +1,15 @@
+package com.hac.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by tnkhang on 3/21/2017.
+ */
+@Getter @Setter
+public class Video {
+  private String interactionId;
+  private String location;
+  private String name;
+  private int resolution;
+}
